681e250d5640ee517ab9c6a67e98c3594a2768c6
apolanco18 <39174984+apolanco18@users.noreply.github.com>
2019-03-05 14:34:30 -0500

    Person Model
    created the models for the people info we are storing

---
+++ src/app/person/person.ts
@@ -0,0 +1,10 @@
+export interface Person{
+    name: string;
+    dateBirth: number;
+    age: number;
+    gender: string;
+    countryOrigin: string;
+    language: string;
+    countryResidence: string;
+    firstContactDate: string;
+}
