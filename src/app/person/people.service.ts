481bced3d3b9100f0d0bd4671b9c269ecfafced2
morutkin <morutkin@gmail.com>
2019-03-16 15:26:56 -0400

    ID added to person model

--- src/app/person/people.service.ts
+++ src/app/person/people.service.ts
@@ -21,9 +21,7 @@ export class PeopleService{
         .pipe(map((postData) => {
             return postData.person.map(person => {
                 return {
-                    title: person.title,
-                    content: person.content,
-                    id: person._id
+                   
 
                     name: person.name,
                     dateBirth: person.dateBirth,
@@ -50,7 +48,7 @@ export class PeopleService{
         return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
     }
 
-    addPost(title: string, content: string){
+    addPerson(title: string, content: string){
         const post: Post = {id: null, title: title, content: content}
         this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
         .subscribe( responseData => {
@@ -62,27 +60,5 @@ export class PeopleService{
         });
     }
 
-    updatePost(id: string, title: string, content: string){
-        const post: Post = { id: id, title: title, content: content }
-        this.http.put("http://localhost:3000/api/posts/" + id, post)
-        .subscribe(response => {
-            console.log(response);
-            const updatedPosts = [...this.posts];
-            const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
-            updatedPosts[oldPostIndex] = post;
-            this.posts = updatedPosts;
-            this.postsUpdated.next([...this.posts]);
-            this.router.navigate(["/"]);
-
-        });
-    }
-
-    deletePost(postId: string){
-        this.http.delete("http://localhost:3000/api/posts/" + postId)
-        .subscribe(() =>{
-            const updatedPosts = this.posts.filter(post => post.id !== postId);
-            this.posts = updatedPosts;
-            this.postsUpdated.next([...this.posts]);
-        });
-    }
+    
 }
