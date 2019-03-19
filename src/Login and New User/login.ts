afbb93b23f43de80bd6cb8be90999270537f35e0
Julie Wang <julie.wang@emory.edu>
2019-03-19 15:32:47 -0400

    Some more edits to Login and New User TS

---
+++ src/Login and New User/login.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth.service'
+@Component({
+   selector: 'app-login'
+   templateUrl: './login.html'
+   styleUrls:['./login.css']
+})
+export class LoginComponent implements OnInit {
+
+    constructor{private Auth: AuthService} {  }
+
+    ngOnInit{} {
+    }
+
+    loginUser(event) {
+      event.preventDefault()
+      const target = event.target
+      const username = target.querySelector('#username').value
+      const password = target.querySelector('#password').value
+      console.log(username, password)
+    }
+}
---
+++ src/Login and New User/new_user.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth.service'
+@Component({
+   selector: 'app-newuser'
+   templateUrl: './new_user.html'
+   styleUrls:['./new_user.css']
+})
+export class LoginComponent implements OnInit {
+
+    constructor{private Auth: AuthService} {  }
+
+    ngOnInit{} {
+    }
+
+    loginUser(event) {
+      event.preventDefault()
+      const target = event.target
+      const username = target.querySelector('#username').value
+      const password = target.querySelector('#password').value
+      console.log(username, password)
+    }
+}
